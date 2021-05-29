9bf72a77506e08c813a119113d708e9ff32e0d15
Igor Milenkovic <milenkovicig@gmail.com>
2021-05-29 10:36:14 +0200

    Display message from server to frontend

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,31 @@
+const chatForm = document.getElementById("chat-form");
+
 const socket = io();
+
+// Display message sent from server
+socket.on("message", (message) => {
+  console.log(message);
+  outputMessage(message);
+});
+
+// Message submit
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // get value of input with id "msg"
+  const msg = e.target.elements.msg.value;
+
+  // emit message to server
+  socket.emit("chatMessage", msg);
+});
+
+// Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    <p class="text">
+        ${message}
+    </p>`;
+  document.querySelector(".chat-messages").appendChild(div);
+}
--- server.js
+++ server.js
@@ -13,6 +13,24 @@ app.use(express.static(path.join(__dirname, "public")));
 // Run when client connects -> listen for event "connection"
 io.on("connection", (socket) => {
   console.log("New WS Connection...");
+
+  // Send message from server -> Welcome single client that connecting
+  socket.emit("message", "Welcome to Chat application :)");
+
+  // Broadcast when a user connects -> emit to everyone, not including client that's connecting
+  socket.broadcast.emit("message", "A user has joind the chat");
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    // Emit to everyone
+    io.emit("message", "A user has left the chat");
+  });
+
+  // Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    // emit back to client -> everybody
+    io.emit("message", msg);
+  });
 });
 
 const PORT = 3000 || process.env.PORT;
